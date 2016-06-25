8e2a3b7f62f40e4f5b94e536b85599dd92d82ff1
Keith Brandenburg <windweaver828@gmail.com>
2016-06-25 00:49:11 -0500

    Stream images instead of touching disk

--- getradar.py
+++ getradar.py
@@ -2,7 +2,9 @@
 
 from bs4 import BeautifulSoup
 from urllib2 import urlopen, Request, HTTPError, URLError
-from urllib import urlretrieve
+import requests
+import cStringIO
+import Image
 
 
 def make_soup(url):
@@ -28,15 +30,23 @@ def get_radar(url, out_folder):
         if not filename.endswith('.gif'):
             continue
         imageurl = url + filename
-        outpath = IMAGEPATH + filename
+        response = requests.get(imageurl)
         try:
-            urlretrieve(imageurl, outpath)
+            foreground = Image.open(cStringIO.StringIO(response.content))
         except Exception as e:
             print(e)
-            print "skipping" + imageurl
+            print "skipping " + imageurl
+            return
+        background = Image.open("basemap.jpg")
+        foreground = foreground.convert('RGBA')
+        background.paste(foreground, (0, 0), foreground)
+        # background = background.convert('P')
+        background.save(out_folder + filename)
 
 if __name__ == "__main__":
     import os
     RADARSITE = 'http://radar.weather.gov/ridge/RadarImg/N1P/DGX/'
     IMAGEPATH = os.path.expanduser('~/Projects/weather/images/')
+    for f in [IMAGEPATH + f for f in os.listdir(IMAGEPATH) if f.endswith(".gif")]:
+        os.remove(f)
     get_radar(RADARSITE, IMAGEPATH)
